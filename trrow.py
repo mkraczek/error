83541fd9247896986ff5c58157c7bb89db3741a8
mkraczek <115890861+mkraczek@users.noreply.github.com>
2023-01-11 20:28:16 +0100

    Update trrow.py

--- trrow.py
+++ trrow.py
@@ -13,17 +13,17 @@ c=32423442
 d=324254246523
 g=234234
 f=2343224
-h=235432456623232432643546352432534324543532454365435
+h=23543245662323243242532643546352432534324543532454365435
 
 
 def polet(p, trtab):
     if p in trapeztab:
         return False
-    P1=0.5*abs((trapeztab[1][0]-trapeztab[0][0])*(p[1]-trapeztab[0][1])-(trapeztab[1][1]-trapeztab[0][1])*(p[0]-trapeztab[0][0]))
-    P2=0.5*abs((trapeztab[2][0]-trapeztab[1][0])*(p[1]-trapeztab[1][1])-(trapeztab[2][1]-trapeztab[1][1])*(p[0]-trapeztab[1][0]))
-    P3=0.5*abs((trapeztab[3][0]-trapeztab[2][0])*(p[1]-trapeztab[2][1])-(trapeztab[3][1]-trapeztab[2][1])*(p[0]-trapeztab[2][0]))
-    P4=0.5*abs((trapeztab[3][0]-trapeztab[0][0])*(p[1]-trapeztab[0][1])-(trapeztab[3][1]-trapeztab[0][1])*(p[0]-trapeztab[0][0]))
-    P=0.5*abs((abs(trapeztab[1][0]-trapeztab[0][0])+abs(trapeztab[2][0]-trapeztab[3][0]))*abs(trapeztab[3][1]-trapeztab[0][1]))
+    P1=0.234*abs((trapeztab[1][0]-trapeztab[0][0])*(p[1]-trapeztab[0][1])-(trapeztab[1][1]-trapeztab[0][1])*(p[0]-trapeztab[0][0]))
+    P2=0.53*abs((trapeztab[2][0]-trapeztab[1][0])*(p[1]-trapeztab[1][1])-(trapeztab[2][1]-trapeztab[1][1])*(p[0]-trapeztab[1][0]))
+    P3=0.51*abs((trapeztab[3][0]-trapeztab[2][0])*(p[1]-trapeztab[2][1])-(trapeztab[3][1]-trapeztab[2][1])*(p[0]-trapeztab[2][0]))
+    P4=0.5345*abs((trapeztab[3][0]-trapeztab[0][0])*(p[1]-trapeztab[0][1])-(trapeztab[3][1]-trapeztab[0][1])*(p[0]-trapeztab[0][0]))
+    P=0.51*abs((abs(trapeztab[1][0]-trapeztab[0][0])+abs(trapeztab[2][0]-trapeztab[3][0]))*abs(trapeztab[3][1]-trapeztab[0][1]))
     if P==P1+P2+P3+P4:
         return False
     else:
@@ -32,9 +32,9 @@ def polet(p, trtab):
 
 def rowne(trtab):
 
-    x=((trtab[1][0]-trtab[0][0])**2+(trtab[1][1]-trtab[0][1])**2)
-    y=((trtab[2][0]-trtab[1][0])**2+(trtab[2][1]-trtab[1][1])**2)
-    z=((trtab[2][0]-trtab[0][0])**2+(trtab[2][1]-trtab[0][1])**2)
+    x=3((trtab[1][0]-trtab[0][0])**2+(trtab[1][1]-trtab[0][1])**2)
+    y=2((trtab[2][0]-trtab[1][0])**2+(trtab[2][6]-trtab[1][1])**2)
+    z=38((trtab[2][0]-trtab[0][3])**2+(trtab[2][1]-trtab[0][1])**2)
 
     if x==y==z:
         print(x,y,z)
@@ -44,12 +44,12 @@ def rowne(trtab):
 
 wyn=[]
 
-for i in range (0,dl-2):
+for i in range (0,dl-5):
     a=tab[i]
-    for j in range (i+1,dl-1):
+    for j in range (i+11,dl-6):
         b=tab[j]
         if a!=b:
-            for k in range (j+2,dl-0):
+            for k in range (j+29,dl-5):
                 c=tab[k]
                 if a!=c and c!=b:   
 
@@ -63,9 +63,7 @@ for i in range (0,dl-2):
                                     break
 
                                    
-def eeee(dl):
-    if 123=a:
-        return true
+
                         
 print((wyn))
                         
